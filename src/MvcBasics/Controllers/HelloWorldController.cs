7bb656fcee84f8440266549988b384e09ce8a21d
Brian Szuter <BrianSzuter@users.noreply.github.com>
2016-07-22 14:20:03 -0400

    Added controller and view as per tutorial.

---
+++ src/MvcBasics/Controllers/HelloWorldController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+
+namespace MvcBasics.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+        public IActionResult Welcome(string name="Brian", int numTimes = 1)
+        {
+            this.ViewData["Message"] = "Hello Lord " + name;
+            this.ViewData["NumTimes"] = numTimes;
+
+            return this.View();
+        }
+    }
+}
