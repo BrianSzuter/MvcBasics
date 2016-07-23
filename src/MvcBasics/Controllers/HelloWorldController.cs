ce1aeb97e362b9af3465496d7865397c87ac9914
Brian Szuter <BrianSzuter@users.noreply.github.com>
2016-07-23 12:42:16 -0400

    Added Movie.cs (Model), MoviesController.cs (Controller). Added "Individual User Accounts" authentication by merging in new project with that setting enabled.

--- src/MvcBasics/Controllers/HelloWorldController.cs
+++ src/MvcBasics/Controllers/HelloWorldController.cs
@@ -15,7 +15,7 @@ namespace MvcBasics.Controllers
 
         // 
         // GET: /HelloWorld/Welcome/ 
-        public IActionResult Welcome(string name="Brian", int numTimes = 1)
+        public IActionResult Welcome(string name="Anonymous", int numTimes = 1)
         {
             this.ViewData["Message"] = "Hello Lord " + name;
             this.ViewData["NumTimes"] = numTimes;
