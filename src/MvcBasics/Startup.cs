0a977c782b26a50ade54359916dc3cdde5855995
Brian Szuter <BrianSzuter@users.noreply.github.com>
2016-07-23 13:58:56 -0400

    Added SeedData for Movies DB.

--- src/MvcBasics/Startup.cs
+++ src/MvcBasics/Startup.cs
@@ -83,6 +83,8 @@ namespace MvcBasics
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            SeedData.Initialize(app.ApplicationServices);
         }
     }
 }
